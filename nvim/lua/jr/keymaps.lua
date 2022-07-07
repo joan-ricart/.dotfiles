8ed7931847c828b1c456cb455b78f92f28f7eb29
joan-ricart <86612520+joan-ricart@users.noreply.github.com>
2022-07-07 17:29:54 +0200

    Update keymaps.lua

--- nvim/lua/jr/keymaps.lua
+++ nvim/lua/jr/keymaps.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 
-vim.keymap.set({'n', 'v'}, '<Leader>y', '"*y')
+vim.keymap.set('n', '<Leader>ff', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<Leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<Leader>fb', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<Leader>fh', '<cmd>Telescope help_tags<cr>')
